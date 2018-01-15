3a51e5b45cf78ebda8e34f225f779dc5d2fcdad4
Nik Ansell <nikansell00@gmail.com>
2018-01-15 20:43:59 +0400

    Added cminer v10

--- start-eth-cminer.sh
+++ start-eth-cminer.sh
@@ -7,6 +7,12 @@ WALLET=0x75A3CdA475EE196916ec76C7174eCd7886163beC
 WORKER=gtx-1060x4-cminer
 EMAIL=nikansell00@gmail.com
 POOLPASS=x
+POWERLIMIT_WATTS=78
+GPUOVERCLOCK=107
+MEMOVERCLOCK=935
+LIMITPOWER='yes'
+#CMINERDIR='~/cminer9.4'
+#CMINERDIR='~/cminer10.0'
 
 #export GPU_FORCE_64BIT_PTR=0
 #export GPU_MAX_HEAP_SIZE=100
@@ -14,7 +20,75 @@ POOLPASS=x
 #export GPU_MAX_ALLOC_PERCENT=100
 #export GPU_SINGLE_ALLOC_PERCENT=100
 
-cd ~/cminer9.4
+#   ------------------------
+#   Configure Nvidia cards
+#   ------------------------
+
+NUMGPU="$(nvidia-smi -L | wc -l)"
+
+#   Power Limit
+#   Commented out as this needs root
+#   .. will figure this out later
+
+if [ $LIMITPOWER = 'yes' ] ; then
+
+   echo "Limiting power to $POWERLIMIT_WATTS"
+   sudo nvidia-smi -pm 1
+   sudo nvidia-smi -pl $POWERLIMIT_WATTS
+
+fi
+
+#   GPU Overlock
+
+if [ $GPUOVERCLOCK = 'yes' ] ; then
+
+   echo "Overclocking GPU by $GPUOVERCLOCK"
+   n=0
+   while [ $n -lt $NUMGPU ];
+   do
+      nvidia-settings -a [gpu:${n}]/GPUGraphicsClockOffset[3]=$GPUOVERCLOCK
+      let n=n+1
+   done
+
+fi
+
+#   GPU Memory Overclock
+
+if [ $GPUMEMOVERCLOCK = 'yes' ] ; then
+
+   echo "Overclocking GPU Memory"
+   n=0
+   while [ $n -lt $NUMGPU ];
+   do
+      nvidia-settings -a [gpu:${n}]/GPUMemoryTransferRateOffset[3]=$MEMOVERCLOCK
+      let n=n+1
+   done
+
+fi
+
+#   Set fan speed to auto
+
+#n=0
+#while [  $n -lt  $NUMGPU ];
+#do
+#    nvidia-settings -a [gpu:${n}]/GPUFanControlState=0
+#    let n=n+1
+#done
+
+#   Set max performance mode
+
+#n=0
+#while [  $n -lt  $NUMGPU ];
+#do
+#    nvidia-settings -a [gpu:${n}]/GPUPowerMizerMode=1
+#    let n=n+1
+#done
+
+#   ------------------------
+#   Start mining
+#   ------------------------
+
+cd ~/cminer10.0
 
 #   Delete files older than LOGFILERETENTIONDAYS
 find ./*_log.txt -mtime +$LOGFILERETENTIONDAYS -exec rm {} \;
@@ -26,5 +100,5 @@ echo "POOL: $SERVER2, WALLET: $WALLET.$WORKER/$EMAIL, WORKER: $WORKER, ESM: 0, A
 #   Start nvidia logging
 ~/mining-scripts/nvid-mon.sh &
 
-#./ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal 0x75a3cda475ee196916ec76c7174ecd7886163beC.gtx-1060x4-cminer/nikansell00@gmail.com -epsw x -mode 1 -ftime 10
-./ethdcrminer64 -epool $SERVER1 -ewal $WALLET.$WORKER/$EMAIL -epsw $POOLPASS -mode 1 -ftime 10
+#./ethdcrminer64 -epool eth-eu1.nanopool.org:9999 -ewal 0x75a3cda475ee196916ec76c7174ecd7886163beC.gtx-1060x4-cminer/nikansell00@gmail.com -epsw x -mode 1 -ftime 10 -ttli 80
+./ethdcrminer64 -epool $SERVER1 -ewal $WALLET.$WORKER/$EMAIL -epsw $POOLPASS -mode 1 -ftime 10 -ttli 80
