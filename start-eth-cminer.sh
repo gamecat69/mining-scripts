26c24dd000f780f1a5e5bdd5bab6eb0429953db4
gamecat69 <nikansell00@gmail.com>
2018-02-27 22:38:53 +0400

    Updated start-eth-cminer.sh to use config.json

--- start-eth-cminer.sh
+++ start-eth-cminer.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
+function readJson {
+
+	UNAMESTR=`uname`
+	if [[ "$UNAMESTR" == 'Linux' ]]; then
+    	SED_EXTENDED='-r'
+	elif [[ "$UNAMESTR" == 'Darwin' ]]; then
+    	SED_EXTENDED='-E'
+	fi;
+
+	VALUE=`grep -m 1 "\"${2}\"" ${1} | sed ${SED_EXTENDED} 's/^ *//;s/.*: *"//;s/",?//'`
+
+	if [ ! "$VALUE" ]; then
+		echo "Error: Cannot find \"${2}\" in ${1}" >&2;
+		exit 1;
+	else
+		echo $VALUE ;
+	fi;
+
+}
+
 WORKINGDIR=/home/mining/mining-scripts
 cd $WORKINGDIR
 
+SERVER1=`readJson config.json SERVER1`
+SERVER2=`readJson config.json SERVER2`
+ETHWALLET=`readJson config.json ETHWALLET`
+WORKER=`readJson config.json MINERNAME`
+EMAIL=`readJson config.json EMAIL`
+POOLPASS=`readJson config.json POOLPASS`
+CMINERDIR=`readJson config.json CMINERDIR`
+LOGFILERETENTIONDAYS=`readJson config.json LOGFILERETENTIONDAYS`
+
 #	Load variables from config file
-source ./config.sh
+#source ./config.sh
 
 #SERVER1=eth-eu1.nanopool.org:9999
 #SERVER2=eth-eu2.nanopool.org:9999
@@ -21,7 +50,7 @@ MININGCMD="./ethdcrminer64 -epool $SERVER1 -ewal $ETHWALLET.$WORKER/$EMAIL -epsw
 cd ~/$CMINERDIR
 
 #   Delete files older than LOGFILERETENTIONDAYS
-find ./*_log.txt -mtime +$LOGFILERETENTIONDAYS -exec rm {} \;
+find $CMINERDIR/*_log.txt -mtime +$LOGFILERETENTIONDAYS -exec rm {} \;
 
 #   Init epools.txt
 echo "POOL: $SERVER1, WALLET: $ETHWALLET.$WORKER/$EMAIL, WORKER: $WORKER, ESM: 0, ALLPOOLS: 0" > epools.txt
