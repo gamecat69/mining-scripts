d153ccc06f76704262412d662cad348956caa494
gamecat69 <nikansell00@gmail.com>
2018-02-27 22:51:42 +0400

    Updated to use config.json

--- start.sh
+++ start.sh
@@ -1,17 +1,48 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-PUSH_MSG="Starting up...\
-Check here for report: http://min-mon.s3-website-eu-west-1.amazonaws.com"
-PUSH_TITLE="gtx-1060x6"
-WAITSECS=20
+#	-------------------------------------
+#	Function to get data from json file
+#	-------------------------------------
+
+function readJson {
+
+	UNAMESTR=`uname`
+	if [[ "$UNAMESTR" == 'Linux' ]]; then
+    	SED_EXTENDED='-r'
+	elif [[ "$UNAMESTR" == 'Darwin' ]]; then
+    	SED_EXTENDED='-E'
+	fi;
+
+	VALUE=`grep -m 1 "\"${2}\"" ${1} | sed ${SED_EXTENDED} 's/^ *//;s/.*: *"//;s/",?//'`
+
+	if [ ! "$VALUE" ]; then
+		echo "Error: Cannot find \"${2}\" in ${1}" >&2;
+		exit 1;
+	else
+		echo $VALUE ;
+	fi;
+
+}
 
 WORKINGDIR=/home/mining/mining-scripts
 cd $WORKINGDIR
 
+RED='\033[0;31m'
+YELLOW='\033[0;93m'
+NC='\033[0m' # No Color
+
+MINERNAME=`readJson config.json MINERNAME`
+PUSH_MSG=`readJson config.json PUSH_MSG`
+MINMON_DELAY_SECS=`readJson config.json MINMON_DELAY_SECS`
+MINE_XMR=`readJson config.json MINE_XMR`
+MINE_ETH=`readJson config.json MINE_ETH`
+
+#PUSH_MSG="Starting up...\
+#Check here for report: http://min-mon.s3-website-eu-west-1.amazonaws.com"
+#PUSH_TITLE="gtx-1060x6"
+
 #	Get config data
-source ./config.sh
+#source ./config.sh
 
 echo -e "${RED}Killing previous processes...${NC}"
 ./kill-miner.sh
@@ -40,9 +71,9 @@ if [ "$MINE_XMR" = "yes" ] ; then
 fi
 
 echo "Sending pushover message"
-./pushover.sh "$PUSH_TITLE" "$PUSH_MSG"
+./pushover.sh "$MINERNAME" "$PUSH_MSG"
 
-sleep $WAITSECS
+sleep $MINMON_DELAY_SECS
 
 echo "Starting min-mon"
 ./min-mon.sh
