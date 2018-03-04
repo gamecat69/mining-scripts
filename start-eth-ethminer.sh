38136d0ceb4070d0946ab3108ac2b395e9ed35e3
Nik Ansell <nikansell00@gmail.com>
2018-03-04 16:21:11 +0400

    added api monitoring switch to ethminer

--- start-eth-ethminer.sh
+++ start-eth-ethminer.sh
@@ -4,37 +4,6 @@
 #   Pre-reqs
 #   ---------------------
 
-#   Before running this script do the following to allow nvidia tweaks
-#   sudo nvidia-xconfig --enable-all-gpus
-#   sudo nvidia-xconfig --cool-24=bits
-#   reboot
-
-#   To run this script as a service:
-
-#vi /lib/systemd/system/ethminer.service
-
-#[Unit]
-#Description=Ethminer mining daemon
-#After=network.target
-
-#[Service]
-#User=mining
-#Group=mining
-#WorkingDirectory=/home/mining/ethminer
-#Type=simple
-#ExecStart=/home/mining/ethminer/start.sh
-#GuessMainPID=no
-
-#[Install]
-#WantedBy=multi-user.target
-
-#systemctl daemon-reload
-#systemctl enable ethminer
-#systemctl start ethminer
-
-#To follow progress :
-#journalctl -fa -u ethminer
-
 #   ----------------
 #   Set variables
 #   ----------------
@@ -42,83 +11,19 @@
 SERVER=http://eth1.nanopool.org:8888
 FSERVER=http://eth-eu2.nanopool.org:9999
 WALLET=0x75A3CdA475EE196916ec76C7174eCd7886163beC
-WORKER=gtx-1060x4-ethminer
+WORKER=gtx-1060x6-2-ethminer
 EMAIL=nikansell00@gmail.com
-POWERLIMIT_WATTS=78
-GPUOVERCLOCK=107
-MEMOVERCLOCK=935
-LIMITPOWER='yes'
-MININGCMD="ethminer -G -F $SERVER/$WALLET.$WORKER/$EMAIL --farm-recheck 200"
+MININGCMD="/home/mining/ethminer/bin/ethminer --opencl -U -F $SERVER/$WALLET.$WORKER/$EMAIL --farm-recheck 200 --api-port 3333"
 
-#export GPU_FORCE_64BIT_0=PTR
-#export GPU_MAX_HEAP_SIZE=100
-#export GPU_USE_SYNC_OBJECTS=1
-#export GPU_MAX_ALLOC_PERCENT=100
-#export GPU_SINGLE_ALLOC_PERCENT=100
+export GPU_FORCE_64BIT_0=PTR
+export GPU_MAX_HEAP_SIZE=100
+export GPU_USE_SYNC_OBJECTS=1
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_SINGLE_ALLOC_PERCENT=100
 
-#   ------------------------
-#   Configure Nvidia cards
-#   ------------------------
-
-NUMGPU="$(nvidia-smi -L | wc -l)"
-
-#   Power Limit
-#   Commented out as this needs root
-#   .. will figure this out later
-
-if [ "$LIMITPOWER" = "yes" ] ; then
-
-   echo "Limiting power to $POWERLIMIT_WATTS"
-   sudo nvidia-smi -pm 1
-   sudo nvidia-smi -pl $POWERLIMIT_WATTS
-
-fi
-
-#   GPU Overlock
-
-if [ "$GPUOVERCLOCK" = "yes" ] ; then
-
-   echo "Overclocking GPU by $GPUOVERCLOCK"
-   n=0
-   while [ $n -lt $NUMGPU ];
-   do
-      nvidia-settings -a [gpu:${n}]/GPUGraphicsClockOffset[3]=$GPUOVERCLOCK
-      let n=n+1
-   done
-
-fi
-
-#   GPU Memory Overclock
-
-if [ "$GPUMEMOVERCLOCK" = "yes" ] ; then
-
-   echo "Overclocking GPU Memory"
-   n=0
-   while [ $n -lt $NUMGPU ];
-   do
-      nvidia-settings -a [gpu:${n}]/GPUMemoryTransferRateOffset[3]=$MEMOVERCLOCK
-      let n=n+1
-   done
-
-fi
-
-#   Set fan speed to auto
-
-#n=0
-#while [  $n -lt  $NUMGPU ];
-#do
-#    nvidia-settings -a [gpu:${n}]/GPUFanControlState=0
-#    let n=n+1
-#done
-
-#   Set max performance mode
-
-#n=0
-#while [  $n -lt  $NUMGPU ];
-#do
-#    nvidia-settings -a [gpu:${n}]/GPUPowerMizerMode=1
-#    let n=n+1
-#done
+#	Kill previous process
+echo "Killing previous ethminer process"
+pkill -f "ethminer -U"
 
 #   ------------------------
 #   Start mining
