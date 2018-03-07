bd5f55e02986ecbc2b906d4759b0a4529d32a87f
gamecat69 <nikansell00@gmail.com>
2018-03-07 21:13:45 +0400

    Added BTCP worker stats

--- min-mon.py
+++ min-mon.py
@@ -515,9 +515,9 @@ def getEarnedCoins():
 	for payment in js["data"]:
 		ethEarned = ethEarned + payment["amount"]
 	
-	ethEarned  = "{0:.4f}".format(ethEarned)
-	btcpEarned = "{0:.4f}".format(btcpEarned)
-	xmrEarned  = "{0:.4f}".format(xmrEarned)
+	ethEarned  = "{0:.4f}".format(float(ethEarned))
+	btcpEarned = "{0:.4f}".format(float(btcpEarned))
+	xmrEarned  = "{0:.4f}".format(float(xmrEarned))
 
 	print("[MIN MON] ethEarned: %s" % ethEarned)
 	print("[MIN MON] btcpEarned: %s" % btcpEarned)
