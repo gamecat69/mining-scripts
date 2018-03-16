344e3949030252396065b6173fde2ec8f665e8e4
gamecat69 <nikansell00@gmail.com>
2018-03-16 21:02:32 +0400

    Updated xmr variable names

--- start-xmr.sh
+++ start-xmr.sh
@@ -32,11 +32,11 @@ function output {
 WORKINGDIR=/home/mining/mining-scripts
 cd $WORKINGDIR
 
-XMRWALLET=`readJson config.json XMR_WALLET`
-XMRPOOL=`readJson config.json XMR_POOL`
-XMRPOOLPASS=`readJson config.json XMR_POOLPASS`
-XMRCPUONLY=`readJson config.json XMR_CPUONLY`
-XMRCURRENCY=`readJson config.json XMR_CURRENCY`
+XMRWALLET=`readJson config.json XMRWALLET`
+XMRPOOL=`readJson config.json XMRPOOL`
+XMRPOOLPASS=`readJson config.json XMRPOOLPASS`
+XMRCPUONLY=`readJson config.json XMRCPUONLY`
+XMRCURRENCY=`readJson config.json XMRCURRENCY`
 
 #XMR_WALLET=45kbjV6VRZ3GMPBHktLh9VGgun5nxEwtXS18yL8S23Gu8gvsE31JtMPHo6DwTwi4s4he3r6U5pmmo1ZhMVgsKrECPH3gBsk
 #XMR_POOL=pool.minexmr.com:7777
