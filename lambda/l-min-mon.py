587e9ed3e5190f56a76e99c89287488f2889ff45
gamecat69 <nikansell00@gmail.com>
2018-03-16 11:35:51 +0400

    Cleaned up output and normalized into functions

--- lambda/l-min-mon.py
+++ lambda/l-min-mon.py
@@ -19,27 +19,72 @@ maxAge      = int(os.environ['MAX_AGE'])
 minHashRate = int(os.environ['MIN_HASHRATE'])
 host        = os.environ['TPLINK_HOST']
 token       = os.environ['TPLINK_API_TOKEN']
+testMode    = os.environ['TEST_MODE']
+debug       = os.environ['DEBUG_MODE']
 
 tpLinkUrl   = host + "?token=" + token
 headers     = {"Content-Type": "application/json"}
 powerStates = ['off','on']
 
-
 #	--------------------------------
 #	To do
 
-#	- Create checkFileAge()
-#	- Create checkHashRate()
-#	- Create updateError()
-#	- Create powerCycleRig()
-#	- Add DEBUG env var. Print out debug info if == 1
-#	- Skip hashrate check if file is too old
 #	- Create HTML report files using json in /nodes/<minername>-monitor.json
 #	- Get CoinUSD values? - possible new Lambda
 #	- Get current Coin balances? - possible new Lambda
 #	--------------------------------
 
-def updateConfig(data, key):
+def debugOutput(str):
+	if debug == 'true':
+		print(str)
+
+def powerCycleRig(deviceId):
+	powerCtrlTplinkDevice(deviceId,0)
+	time.sleep(1)
+	powerCtrlTplinkDevice(deviceId,1)
+
+def getS3JsonData(bucket, key, param):
+
+	try:
+		fileObj  = s3.Object(bucket, key)
+		contents = fileObj.get()['Body'].read().decode('utf-8')
+	except:
+		sendMessage("getS3JsonData: Cannot get object S3://%s/%s." % (bucket,key))
+		return False
+
+	try:
+		js    = json.loads(contents)
+		value = int(js[param])
+	except:
+		sendMessage("getS3JsonData: Cannot get %s from object S3://%s/%s." % (param,bucket,key))
+		return False
+
+	return value
+
+def getS3FileAge(bucket, key):
+
+	try:
+		#	Get Last Modified date and time of monitoring data file
+		fileDetails=s3.ObjectSummary(bucket,key)
+	except:
+		sendMessage("getS3FileAge: Cannot get age for object S3://%s/%s. Assuming file not too old" % (bucket,key))
+		return False
+
+	#	Get fileData and convert to timestamp
+	fileDate=fileDetails.last_modified
+	tsFileDate=fileDate.timestamp()
+
+	#	Get the current timestamp and calculate the file age in seconds
+	#	time.time() and datetime.datetime.timestamp() both return a float
+	now = int(time.time())
+	fileAgeSecs = now - int(tsFileDate)
+
+	#formatedTime=fileDate.strftime("%Y-%m-%d %H:%M:%S")
+	#print("[%s] File is %d seconds old. Modified on: %s" % (rig['name'], fileAgeSecs, formatedTime) )
+
+	return fileAgeSecs
+
+def jsonToS3File(data, key):
 
 	client = boto3.client('s3')	
 
@@ -96,8 +141,9 @@ def processJson(data):
 		return ''
 
 def powerCtrlTplinkDevice(deviceId, state):
-	print("powerCtrlTplinkDevice: %s" % deviceId)
-	print("Powering device: %s" % powerStates[state])
+	
+	debugOutput("powerCtrlTplinkDevice: %s" % deviceId)
+	debugOutput("Powering device: %s" % powerStates[state])
 
 	jsonPostData = {
 		"method":"passthrough",
@@ -114,98 +160,88 @@ def powerCtrlTplinkDevice(deviceId, state):
 	if not js:
 		exit (1)
 	else:
-		print("powerCtrlTplinkDevice: Device powered %s" % powerStates[state])
+		debugOutput("powerCtrlTplinkDevice: Device powered %s" % powerStates[state])
 
-def send_message(text):
-	payload = {"message": text, "user": USER, "token": API }
-	r = requests.post('https://api.pushover.net/1/messages.json', data=payload, headers={'User-Agent': 'Python'})
-	return r
+def sendMessage(text):
+
+	debugOutput("Sending message: %s" % (text) )
+	
+	if testMode != 'true':
+		payload = {"message": text, "user": USER, "token": API }
+		r = requests.post('https://api.pushover.net/1/messages.json', data=payload, headers={'User-Agent': 'Python'})
+		return r
 
 def lambda_handler(event, context):
 	
 	try:
-
 		#	Load rig config from json
 		cfg = loadConfig()
-		
-		#	Cycle through each rig in the json
-		for rig in cfg['rigs']:
 
-			#	Skip rig if not configured for monitoring
-			if rig['monitor'] == 'no':
-				break
-
-			print("[%s] Checking rig..." % (rig['name']))
-
-			key=rig['dataFile']        # json file with monitoring data
-			minHashRate=rig['minKHs']  # min acceptable hashrate
-			deviceId=rig['deviceId']   # TP-Link deviceId for power cyclcing
-			lastError=rig['lastError'] # The previous error logged
-
-			#	Get Last Modified date and time of monitoring data file
-			fileDetails=s3.ObjectSummary(bucket,key)
-			fileDate=fileDetails.last_modified
-		
-			#	Convert fileDate to timestamp
-			tsFileDate=fileDate.timestamp()
-		
-			#	Get the current timestamp and calculate the file age in seconds
-			#	time.time() and datetime.datetime.timestamp() both return a float
-			now = int(time.time())
-			fileAgeSecs = now - int(tsFileDate)
-			formatedTime=fileDate.strftime("%Y-%m-%d %H:%M:%S")
-			#print("[%s] File is %d seconds old. Modified on: %s" % (rig['name'], fileAgeSecs, formatedTime) )
-		
-			#	If file is older than maxAge, there is a problem...
-			#	... the rig has not updated the file and  may be down.
-			if fileAgeSecs > maxAge:
-
-				if lastError == '':
-					#	No previous error. Send message and update config file
-					print("[%s] Rig is down. Will wait for one more failure before taking action" % (rig['name']))
-					send_message("[%s] Rig is down. Will wait for one more failure before taking action" % (rig['name']))
-					rig['lastError']="File not updated within time period"
-					updateConfig(cfg, "nodes.json")
-				else:
-					#	Second failure. Take action
-					send_message("[%s] Rig is down. Powercycling" % (rig['name']))
-					powerCtrlTplinkDevice(deviceId,0)
-					time.sleep(2)
-					powerCtrlTplinkDevice(deviceId,1)
-					
-					#	Clear last error and update config file
-					rig['lastError']=""
-					updateConfig(cfg, "nodes.json")
-			else:
-				#	File has been updated recently. Clear error and update config file
-				print("[%s] Rig is up" % (rig['name']))
-				rig['lastError']=""
-				updateConfig(cfg, "nodes.json")
-
-			#	Read the hashrate from the file
-			fileObj  = s3.Object(bucket, key)
-			contents = fileObj.get()['Body'].read().decode('utf-8')
-			js       = json.loads(contents)
-			hashrate = int(js['ethhashrate'])
-			print("[%s] Hashrate: %s" % (rig['name'], hashrate))
-			
-			#	Raise alert is hashrate too low
-			if hashrate < minHashRate:
-				
-				if lastError == '':
-					print("[%s] Hashrate Alert\nHashrate (%d) below threshold (%d).Will wait for one more failure before taking action" % (rig['name'], hashrate, minHashRate))
-					send_message("[%s] Hashrate Alert\nHashrate (%d) below threshold (%d).Will wait for one more failure before taking action" % (rig['name'], hashrate, minHashRate))				
-				else:
-					print("[%s] Hashrate Alert\nHashrate (%d) below threshold (%d). Powercycling" % (rig['name'], hashrate, minHashRate))
-					send_message("[%s] Hashrate Alert\nHashrate (%d) below threshold (%d). Powercycling" % (rig['name'], hashrate, minHashRate))
-					powerCtrlTplinkDevice(deviceId,0)
-					time.sleep(1)
-					powerCtrlTplinkDevice(deviceId,1)
-					rig['lastError']=""
-					updateConfig(cfg, "nodes.json")
-		return
 	except Exception as e:
+		#	Failed to load config from json
 		print(e)
-		print('Error getting details from object {} in bucket {}.'.format(key, bucket))
+		print('Unable to load config from s3://%s/%s' % (bucket, key))
 		raise e
 		return 'error'
+		
+	#	We have data. Cycle through each rig in the json
+	for rig in cfg['rigs']:
+
+		#	Skip rig if not configured for monitoring
+		if rig['monitor'] == 'no':
+			break
+
+		print("[%s] Checking rig..." % (rig['name']))
+
+		dataFile=rig['dataFile']   # json file with monitoring data
+		minHashRate=rig['minKHs']  # min acceptable hashrate
+		deviceId=rig['deviceId']   # TP-Link deviceId for power cyclcing
+		lastError=rig['lastError'] # The previous error logged
+
+		#	Find out when the data file was last updated	
+		fileAgeSecs = getS3FileAge(bucket, dataFile) or 0
+		debugOutput("[%s] Last update was %d secs ago" % (rig['name'], fileAgeSecs))
+	
+		#	If file is older than maxAge, there is a problem...
+		#	... the rig has not updated the file and  may be down.
+		if fileAgeSecs > maxAge:
+			
+			if testMode == 'true':
+				debugOutput("[%s] (Test Mode) Rig is down. Powercycling" % (rig['name']))
+			else:
+				sendMessage("[%s] Rig is down. Powercycling" % (rig['name']))
+				powerCycleRig(deviceId)
+			
+			#	Add error to data file
+			rig['lastError']="Data file not updated within maximum time configured"
+			jsonToS3File(cfg, "nodes.json")
+
+		else:
+
+			#	File has been updated recently. Check the hashrate
+			print("[%s] Rig is up. Checking hashrate" % (rig['name']))
+
+			#	Read the hashrate from the file
+			hashrate = getS3JsonData(bucket, dataFile, 'ethhashrate') or 0
+			print("[%s] Hashrate: %s" % (rig['name'], hashrate))
+		
+			if hashrate < minHashRate:
+			
+				#	Raise alert. Hashrate too low
+				sendMessage("[%s] Hashrate Alert\nHashrate (%d) below threshold (%d). Powercycling" % (rig['name'], hashrate, minHashRate))
+				
+				if testMode == 'true':			
+					debugOutput("[%s] (Test Mode) Hashrate Alert. Powercycling" % (rig['name']))
+				else:	
+					powerCycleRig(deviceId)
+
+				#	Add error to data file
+				rig['lastError']="Hash rate too low"
+				jsonToS3File(cfg, "nodes.json")
+			else:
+				#	Update last error in data file
+				rig['lastError']=""
+				jsonToS3File(cfg, "nodes.json")					
+
+	#	All rigs processes. quit function
+	return
