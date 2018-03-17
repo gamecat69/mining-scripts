6bc1b2dfb053481372525a824c666e82cb452718
gamecat69 <nikansell00@gmail.com>
2018-03-17 19:35:17 +0400

    Removed global variables

--- min-mon.py
+++ min-mon.py
@@ -271,9 +271,9 @@ def getxmrStakData():
 	#xmrUptimeMin  = int(xmrJson["connection"]["uptime"] / 60)
 	#xmrUptime     = formatUptimeMins(xmrJson["connection"]["uptime"] / 60)
 
-	#xmrtotalshares = 0
-	#xmrtotalshares = int(xmrJson["results"]["shares_good"])
-	int(data['xmrtotalshares']) = xmrJson["results"]["shares_good"]
+	xmrtotalshares = int(xmrJson["results"]["shares_good"])
+	data['xmrtotalshares'] = xmrtotalshares
+	#data['xmrtotalshares'] = xmrJson["results"]["shares_good"]
 	data['xmrversion']     = xmrJson["version"]
 	data['xmrhashrate']    = int(xmrJson["hashrate"]["total"][0]) or 0
 	data['xmrpool']        = xmrJson["connection"]["pool"]
